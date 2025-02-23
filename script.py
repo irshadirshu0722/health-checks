df76786fb6aaaeac74f1b4f5634efed851c0db6f
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-02-23 22:25:56 +0530

    Iterate over a list of checks and messages

--- script.py
+++ script.py
@@ -1,11 +1,17 @@
 def main():
+  checks = [
+    (check_root_full,"Root partition full")
+  ]
+  for check,msg in checks:
+    print(msg)
   print("Everything fine!")
   return
 
 
 def check_root_full():
   """ Returns True if the root partition is full, False otherwise"""
-  pass
+  
+
 
 print("Main function is going to run")
 main()
