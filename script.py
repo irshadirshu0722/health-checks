3050cbfe6f6ad8e3bff3896bab3e6b41bf234683
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-02-23 22:28:09 +0530

    Allow printing more than one error message

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
+import sys
 def main():
   checks = [
     (check_root_full,"Root partition full")
   ]
+  everything_ok  = True
   for check,msg in checks:
     print(msg)
+    everything_ok = False
+  if not everything_ok:
+    sys.exit(1)
   print("Everything fine!")
   return
 
