58f9398b51528be548cf7a3adf3979f634e0f811
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-02-23 22:23:58 +0530

    Add new funciton check_root_full

--- script.py
+++ script.py
@@ -2,7 +2,13 @@ def main():
   print("Everything fine!")
   return
 
+
+def check_root_full():
+  """ Returns True if the root partition is full, False otherwise"""
+  pass
+
 print("Main function is going to run")
 main()
 print("We are here")
 print("We are not here")
+ 
