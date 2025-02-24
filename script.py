ae8aa76586beeaa810f38bf27466b1ed2896d35f
Muhammed irshad ismail <141476447+irshadirshu0722@users.noreply.github.com>
2025-02-24 17:55:34 +0530

    Update script.py
    Added check_cpu function

--- script.py
+++ script.py
@@ -21,7 +21,8 @@ def main():
 def check_root_full():
   """ Returns True if the root partition is full, False otherwise"""
   
-
+def check_cpu():
+  """Return True if cpu is working fine otherwise False"""
 
 print("Main function is going to run")
 main()
